d5068b9c4a55bec14e4570f69073e7b2fd79ad00
Robson Carvallho <robsonn@outlook.com>
2024-07-06 12:25:08 +0100

    Initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+        <div className="">Hello world</div>
+    </>
+  )
+}
+
+export default App
